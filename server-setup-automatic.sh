98aa8afd44eb2537db5efdfaf861b7376b59b813
Ilia Stepanov <35932926+softlyfear@users.noreply.github.com>
2023-03-07 00:56:49 +0300

    Update server-setup-automatic.sh

--- server-setup-automatic.sh
+++ server-setup-automatic.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-#################################################
-# Configuring the server to run as a root user
-#################################################
-# Automatic setup
-# source <(curl -s https://raw.githubusercontent.com/softlyfear/my-script/main/server-setup-automatic.sh)
-#################################################
+#########################################################
+# Configuring the server to run as root with ssh key only
+#########################################################
+# Automatic setup 
+# source <(https://raw.githubusercontent.com/softlyfear/my_script/main/server-setup-automatic.sh)
+#########################################################
 
 #1 update server
 echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 echo -e "\033[35mStarting to set up\033[97m"
 echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
-echo -e "\033[35mUpdate & Upgrade Server\033[97m"
-sudo apt update && \
-sudo apt upgrade -y && \
 echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
+echo -e "\033[35mUpdate & Upgrade Server\033[97m"
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
+sudo apt update -q && \
+sudo apt upgrade -qy && \
 
 #2 setting up ssh login & disable password login
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 echo -e "\033[35mSetting up ssh login & disable password login\033[97m"
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 if [[ ! -d "/root/.ssh" ]]; then
   mkdir root/.ssh || exit
 fi
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 echo -e "\033[35mEnter your ssh public key[97m"
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 IFS= read -r sshkey
 echo "${sshkey}" > /root/.ssh/authorized_keys
 sudo sed -i 's|^PermitRootLogin .*|PermitRootLogin prohibit-password|' /etc/ssh/sshd_config
@@ -31,16 +36,18 @@ sudo sed -i 's/^#PubkeyAuthentication\s.*$/PubkeyAuthentication yes/' /etc/ssh/s
 echo HostKeyAlgorithms +ssh-rsa >> /etc/ssh/sshd_config
 echo PubkeyAcceptedAlgorithms +ssh-rsa >> /etc/ssh/sshd_config
 sudo systemctl restart sshd
-echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 
 #3 install fail2Ban
-echo -e "\033[35mInstalling Fail2Ban\033[97m"
-sudo apt install -y fail2ban && \
 echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
+echo -e "\033[35mInstalling Fail2Ban\033[97m"
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
+sudo apt install -y fail2ban && \
 
 #4 install and configure Unf
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 echo -e "\033[35mInstalling and configure Unf\033[97m"
-sudo apt-get install -y ufw && \
+echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
+sudo apt install -y ufw
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
@@ -48,4 +55,3 @@ sudo ufw enable
 echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
 echo -e "\033[35mInstallation and setup complete\033[97m"
 echo -e "\033[35m-----------------------------------------------------------------------------------\033[97m"
-
