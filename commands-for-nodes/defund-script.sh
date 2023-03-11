3a00e747c21df1f92b1dd915277a0dde32d3d6ce
softlyfear <softlyfear@gmail.com>
2023-03-11 14:36:54 +0300

    add help function

--- commands-for-nodes/defund-script.sh
+++ commands-for-nodes/defund-script.sh
@@ -23,7 +23,7 @@ delegate() {
 #check balance
 balance() {
   "${project}" q bank balances $("${project}" keys show wallet -a)
-echo -e "\\033[35mDivide by 1000 for integers\033[97m"
+echo -e "\033[35mDivide by 1000 for integers\033[97m"
 }
 
 #check logs
@@ -63,4 +63,21 @@ voting() {
   elif [[ "${selection}" = no ]]; then
     "${project}" tx gov vote "${id}" "${selection}" --from wallet --chain-id "${chainid}" --gas-prices 0.1"${token}" --gas-adjustment 1.5 --gas auto -y
   fi
+}
+
+#list all commands
+help() {
+  echo "\033[35m
+  list commands:
+    add - add a function to the bash profile to run when a user logs in
+    delegate - delegate tokens to yourself
+    balance - check balance
+    logs - check logs
+    status - check the synchronization status and show the last block
+    rewards - receive rewards from all validators
+    unjail - unjail validator
+    restart - restart node
+    voting - vote
+    help - list all commands
+  \033[97m"
 }
