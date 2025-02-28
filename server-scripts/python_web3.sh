79e45058eebb55781ba27545d87a1b704b4463c9
Softly <softlyfear@gmail.com>
2025-02-28 16:47:43 +0300

    python_web3.sh

---
+++ server-scripts/python_web3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+###############################################
+# Automatic setup
+# source <(curl -s https://raw.githubusercontent.com/softlyfear/my-script/refs/heads/main/server-scripts/python_web3.sh)
+###############################################
+
+# Создаём виртуальное окружение в директории .venv
+apt install python3-venv -y
+python3 -m venv .venv
+
+# Активируем виртуальное окружение
+source .venv/bin/activate
+# Выйти deactivate
+
+# Устанавливаем web3 в виртуальное окружение
+pip install web3 -y
+pip install --upgrade web3
